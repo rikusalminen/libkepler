bcf9d055c9b1aecce8170e920c1251fe0fe06781
Riku Salminen <riku.salminen@tkk.fi>
2014-10-19 14:01:18 +0300

    Test intercept
    Reduce maximum true anomaly of intercept position on hyperbolioc orbits.

--- test/intercept_test.c
+++ test/intercept_test.c
@@ -30,7 +30,7 @@ static void elliptical_orbit(
     double a = (ap + pe)/2.0;
     double e = (ap - pe) / (ap + pe);
     double p = a * (1 - e*e);
-    double n = sqrt(mu / a*a*a);
+    double n = sqrt(mu / (a*a*a));
 
     elements->semi_latus_rectum = p;
     elements->eccentricity = e;
@@ -47,7 +47,7 @@ void intercept_simple_test(double *params, int num_params, void *extra_args, str
     // p, e, n, i, an, arg, tau
     double mu = 1.0;
     struct kepler_elements orbit1 = { 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0 };
-    struct kepler_elements orbit2 = { 1.0, 0.0, 1.0, M_PI/8.0, 0.0, 0.0, 0.0 };
+    struct kepler_elements orbit2 = { 1.0, 0.0, 1.0, 5*M_PI/180.0, 0.0, 0.0, 0.0 };
 
     elliptical_orbit(mu, 2.0, 1.0, &orbit1);
     elliptical_orbit(mu, 1.0, 1.0, &orbit2);
@@ -89,7 +89,7 @@ void intercept_test(double *params, int num_params, void *extra_args, struct num
     struct kepler_elements orbit1 = { p1, e1, n1, i, an, arg, 0.0 };
 
     double maxf1 = e1 > 1.0 ?
-        acos(1.0/e1) :       // hyperbolic
+        0.5*acos(1.0/e1) :       // hyperbolic
         (ZEROF(e1-1.0) ?
             4*M_PI/5 :      // parabolic
             M_PI);          // closed orbit
@@ -108,7 +108,7 @@ void intercept_test(double *params, int num_params, void *extra_args, struct num
 
     double e2 = params[7] * 4.0;
     double maxf2 = e2 > 1.0 ?
-        acos(1.0/e2) :       // hyperbolic
+        0.5*acos(1.0/e2) :       // hyperbolic
         (ZEROF(e2-1.0) ?
             4*M_PI/5 :      // parabolic
             M_PI);          // closed orbit
@@ -205,4 +205,13 @@ void intercept_test(double *params, int num_params, void *extra_args, struct num
             kepler_orbit_closed(i == 0 ? &orbit1 : &orbit2),
             "Apoapsis intersect on closed orbit");
     }
+
+    double P1 = kepler_orbit_closed(&orbit1) ?
+        kepler_orbit_period(&orbit1) : (M_PI/2 / kepler_orbit_mean_motion(&orbit1));
+    double P2 = kepler_orbit_closed(&orbit2) ?
+        kepler_orbit_period(&orbit2) : (M_PI/2 / kepler_orbit_mean_motion(&orbit2));
+
+    double dt = P1 + P2;
+    bool hit = intercept_orbit(&orbit1, &orbit2, t0-dt, t0+dt, NULL); // XXX: null
+    ASSERT(hit, "Intercept time interval found");
 }
