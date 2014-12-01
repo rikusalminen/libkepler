e6f3a27e00c71308eece6a85d791bac2cc51f645
Riku Salminen <riku.salminen@tkk.fi>
2014-12-01 20:39:43 +0200

    r, t, f, g, fdot, gdot in universal variables
    [skip ci]

--- include/libkepler/universal.h
+++ include/libkepler/universal.h
@@ -1,13 +1,37 @@
 #ifndef LIBKEPLER_UNIVERSAL_H
 #define LIBKEPLER_UNIVERSAL_H
 
-double universal_r();
-double universal_t();
+double universal_r_cs(
+    double mu,
+    double alpha,
+    double s,
+    double r0, double r0v0,
+    const double *cs);
+
+double universal_t_cs(
+    double mu,
+    double alpha,
+    double s,
+    double r0, double r0v0,
+    const double *cs);
+
+void universal_fg_cs(
+    double mu,
+    double alpha,
+    double s,
+    double r0, double r0v0,
+    const double *cs,
+    double *f, double *g);
+
+void universal_fgdot_cs(
+    double mu,
+    double alpha,
+    double s,
+    double r0, double r0v0,
+    const double *cs,
+    double *fdot, double *gdot);
 
 double universal_guess_s();
 double universal_iterate_s();
-
-double universal_fg();
-double universal_fgdot();
 
 #endif
--- src/universal.c
+++ src/universal.c
@@ -1,2 +1,47 @@
 #include <libkepler/universal.h>
 
+double universal_r_cs(
+    double mu,
+    double alpha,
+    double s,
+    double r0, double r0v0,
+    const double *cs) {
+    (void)alpha;
+    return r0 * cs[0] + r0v0 * s * cs[1] + mu * s*s * cs[2];
+}
+
+double universal_t_cs(
+    double mu,
+    double alpha,
+    double s,
+    double r0, double r0v0,
+    const double *cs) {
+    (void)alpha;
+    return r0 * s * cs[1] + r0v0 * s*s * cs[2] + mu * s*s*s * cs[3];
+}
+
+void universal_fg_cs(
+    double mu,
+    double alpha,
+    double s,
+    double r0, double r0v0,
+    const double *cs,
+    double *f, double *g) {
+    (void)alpha;
+    *f = 1.0 - (mu/r0) * s*s * cs[2];
+    *g = r0 * s * cs[1] + r0v0 * s*s * cs[2];
+    // *g = dt - mu * s*s*s * cs[3];
+}
+
+
+void universal_fgdot_cs(
+    double mu,
+    double alpha,
+    double s,
+    double r0, double r,
+    const double *cs,
+    double *fdot, double *gdot) {
+    (void)alpha;
+    *fdot = -mu / (r0*r) * s * cs[1];
+    *gdot = 1.0 - (mu/r) * s*s * cs[2];
+}
