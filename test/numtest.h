ed80e898b7e3e2fe6e09ffeeebe5148a56a64783
Riku Salminen <riku.salminen@tkk.fi>
2015-01-02 15:34:49 +0200

    numtest: wrap ASSERT macro in do .. while
    Allow if() ASSERT(...); else ASSERT(...);

--- test/numtest.h
+++ test/numtest.h
@@ -5,7 +5,12 @@
 #define EQF(a, b) ((ZEROF(a) && ZEROF(b)) || ZEROF(((a)-(b))*((a)-(b))/((a)*(a) + ((b)*(b)))))
 #define LTF(a, b) ((a) < (b) || EQF((a), (b)))
 
-#define ASSERT(cond, msg, ...) { numtest_assert((cond), test_ctx, __FILE__, __LINE__, __FUNCTION__, (msg), ##__VA_ARGS__); }
+#define ASSERT(cond, msg, ...) \
+    do { \
+        numtest_assert( \
+            (cond), test_ctx, __FILE__, __LINE__, __FUNCTION__, \
+            (msg), ##__VA_ARGS__); \
+    } while(0)
 #define ASSERT_EQF(a, b, msg, ...) ASSERT(EQF((a), (b)), msg, ##__VA_ARGS__)
 #define ASSERT_LTF(a, b, msg, ...) ASSERT(LTF((a), (b)), msg, ##__VA_ARGS__)
 #define ASSERT_RANGEF(x, min, max, msg, ...) ASSERT(LTF((min), (x)) && LTF((x), (max)), msg, ##__VA_ARGS__)
